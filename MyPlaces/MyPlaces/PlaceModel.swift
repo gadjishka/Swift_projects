233703352d530115323345d68f6e792475970ce1
Ballin <gereygadji@gmail.com>
2023-06-23 13:24:54 +0300

    Add Realm Framework Add Save Button func Add Image

--- MyPlaces/MyPlaces/PlaceModel.swift
+++ MyPlaces/MyPlaces/PlaceModel.swift
@@ -5,14 +5,15 @@
 //  Created by Гаджи Герейакаев on 22.06.2023.
 //
 
-import Foundation
+import UIKit
 
 struct Place {
     
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurauntImage: String?
     
     static let restaurantNames = ["Траттория VABENE!", "ДжастаПаста", "Сабор де ла Вида Ресторан", "Любовь Пирогова", "АндерСон", "КТ Биро", "Джумбус", "Кафе Кусочки", "Боэми", "Стейк Хаус Бутчер"]
     
@@ -20,9 +21,10 @@ struct Place {
         var places = [Place]()
         
         for place in restaurantNames {
-            places.append(Place(name: place, location: "Москва", type: "Ресторан", image: place))
+            places.append(Place(name: place, location: "Москва", type: "Ресторан",image: nil, restaurauntImage: place))
         }
         
         return places
     }
+    
 }
